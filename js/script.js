5bf8d677b16b1eedbfdda5c2a7c82a7e84080023
Davide Venturelli <dventurelli90@gmail.com>
2022-03-04 17:55:07 +0100

    fix nomi variabili

--- js/script.js
+++ js/script.js
@@ -23,45 +23,45 @@ console.log('Età:' + età)
 
 console.log('Costo per km = 0,21 euro');
 
-const BigliettoIntero = chilometri * 0.21
+const bigliettoIntero = chilometri * 0.21
 
-console.log('Costo biglietto intero: ' + (BigliettoIntero.toFixed(2) + ' euro'));
+console.log('Costo biglietto intero: ' + (bigliettoIntero.toFixed(2) + ' euro'));
 
 // calcolo costo biglietto con e senza sconto 
 
 if (età < 18) {
     alert('Hai diritto allo sconto del 20% sul prezzo del biglietto intero!')
-    const sconto20 = (BigliettoIntero / 100) * 20;
-    const Biglietto20 = (BigliettoIntero - sconto20);
-    console.log('Costo biglietto scontato del 20%: ' + Biglietto20.toFixed(2) + 'euro');
+    const sconto20 = (bigliettoIntero / 100) * 20;
+    const biglietto20 = (bigliettoIntero - sconto20);
+    console.log('Costo biglietto scontato del 20%: ' + biglietto20.toFixed(2) + 'euro');
 
     document.getElementById('dati').innerHTML =
         '<li> Età passeggero: ' + età + '</li> <li> chilometri percorsi: ' + chilometri + '</li> <li> sconto applicato: 20%</li>';
 
     document.getElementById('biglietto').innerHTML =
-        'Il costo del tuo biglietto è di: <strong>' + Biglietto20.toFixed(2) + '</strong>';
+        'Il costo del tuo biglietto è di: <strong>' + biglietto20.toFixed(2) + '</strong>';
 
 } else if (età > 65) {
     alert('Hai diritto allo sconto del 40% sul prezzo del biglietto intero!')
-    const sconto40 = (BigliettoIntero / 100) * 40;
-    const Biglietto40 = (BigliettoIntero - sconto40);
-    console.log('Costo biglietto scontato del 40%: ' + Biglietto40.toFixed(2) + 'euro');
+    const sconto40 = (bigliettoIntero / 100) * 40;
+    const biglietto40 = (bigliettoIntero - sconto40);
+    console.log('Costo biglietto scontato del 40%: ' + biglietto40.toFixed(2) + 'euro');
 
     document.getElementById('dati').innerHTML =
         '<li> Età passeggero: ' + età + '</li> <li> chilometri percorsi: ' + chilometri + '</li> <li> sconto applicato: 40%</li>';
 
     document.getElementById('biglietto').innerHTML =
-        'Il costo del tuo biglietto è di: <strong>' + Biglietto40.toFixed(2) + '</strong>';
+        'Il costo del tuo biglietto è di: <strong>' + biglietto40.toFixed(2) + '</strong>';
 
 } else {
     alert('Purtoppo non hai diritto ad alcuno sconto :(')
-    console.log('Costo biglietto:' + BigliettoIntero.toFixed(2) + 'euro');
-    document.getElementById('biglietto').innerHTML = 'Il costo del tuo biglietto è di: <strong>' + BigliettoIntero.toFixed(2) + '</strong>';
+    console.log('Costo biglietto:' + bigliettoIntero.toFixed(2) + 'euro');
+    document.getElementById('biglietto').innerHTML = 'Il costo del tuo biglietto è di: <strong>' + bigliettoIntero.toFixed(2) + '</strong>';
 
     document.getElementById('dati').innerHTML =
         '<li> Età passeggero: ' + età + '</li> <li> chilometri percorsi: ' + chilometri + '</li> <li> sconto applicato: nessuno</li>';
 
     document.getElementById('biglietto').innerHTML =
-        'Il costo del tuo biglietto è di: <strong>' + BigliettoIntero.toFixed(2) + '</strong>';
+        'Il costo del tuo biglietto è di: <strong>' + bigliettoIntero.toFixed(2) + '</strong>';
 }
 
